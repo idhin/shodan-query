8f5f73c07d3c074860501dbc5d28f08f21837ede
Khulafaur Rasyidin <khulafaurrasyidin@Macbooks-MBP.netis>
2023-08-27 23:40:56 +0700

    add handling error

--- shodan.sh
+++ shodan.sh
@@ -1,9 +1,16 @@
-
 #!/bin/bash
-for i in {1..3000} #Sampe 3000 halaman
+start_page=362
+for ((i=start_page; i<=3000; i++))
 do
     # Menjalankan perintah Python dan menyimpan hasilnya dalam variabel output
-    output=$(python3 shodan.py --search "'queryshodandisini" --list_ip_port --page $i | tee -a output.txt)
+    output=$(python3 Shodan_So.py --search "queymudisini" --list_ip_port --page $i | tee -a output.txt)
+    
+    # Memeriksa apakah output mengandung "Shodan API Error: Unable to parse JSON response"
+    if echo "$output" | grep -q "Shodan API Error: Unable to parse JSON response"; then
+        echo "Shodan API Error terdeteksi, menunggu 3 detik..."
+        sleep 3
+        continue
+    fi
     
     # Memeriksa apakah output sesuai dengan format "IP:Port" menggunakan regex
     if echo "$output" | grep -Eq '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+$'; then
