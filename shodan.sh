e8cbc0fe14b7ab89f09361777fb68702242c3084
Khulafaur Rasyidin <idhinofficial@gmail.com>
2023-10-28 19:46:28 +0700

    Update shodan.sh
    auto add http method

--- shodan.sh
+++ shodan.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-start_page=1
-for ((i=start_page; i<=3000; i++))
+max_retries=10
+
+IFS=':' # Set pemisah kolon sebagai IFS (Internal Field Separator)
+
+for ((start_page=1; start_page<=3000; start_page++))
 do
-    # Menjalankan perintah Python dan menyimpan hasilnya dalam variabel output
-    output=$(python3 Shodan_So.py --search "querymu" --list_ip_port --page $i | tee -a output.txt)
-    
-    # Memeriksa apakah output mengandung pesan error terkait koneksi atau parsing JSON
-    if echo "$output" | grep -q -E "Shodan API Error: (Unable to parse JSON response|Unable to connect to Shodan)"; then
-        echo "Shodan API Error terdeteksi, menunggu 3 detik..."
-        sleep 3
-        continue
-    fi
-    
-    # Memeriksa apakah output sesuai dengan format "IP:Port" menggunakan regex
-    if echo "$output" | grep -Eq '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+$'; then
-        echo "Halaman ke: $i"
-    else
-        echo "Keluaran bukan berupa IP:Port:"
-        echo "$output"
-        echo "Menghentikan looping."
-        break
-    fi
-    
-    sleep 3
+    while IFS= read -r line; do
+        ip_port=($line)
+        ip=${ip_port[0]}
+        port=${ip_port[1]}
+
+        if [[ -n $ip && -n $port ]]; then
+            if [[ $port == "443" ]]; then
+                url="https://$ip"
+            elif [[ $port == "80" ]]; then
+                url="http://$ip"
+            else
+                url="http://$ip:$port"
+            fi
+
+            echo "URL: $url"
+            echo "$url" >> httpduaratusamazon.txt
+        fi
+
+    done <<< "$(python3 Shodan_So.py --search "query'" --list_ip_port --page $start_page | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+$')"
+
 done
+
+unset IFS # Kembalikan IFS ke nilai default
